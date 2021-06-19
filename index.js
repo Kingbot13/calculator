1fa57cd62486d840e8b43bcfde450704b88081ed
Dylan King <dwkblue51@yahoo.com>
2021-06-19 10:31:13 -0500

    add event listner to numpad and add part of event listener to operators

--- index.js
+++ index.js
@@ -14,8 +14,40 @@ function divide(a, b){
     return a / b;
 };
 
+/* 
 
+    grab nodes for operators
+    add event listeners
+    store number as variable to be used in operate()  
+        a = display.innerHTML;
+    store sign as variable
+        sign = sign.innerHTML;
+    store next number as variable
+        b = display.innerHTML;
+    equal sign finishes calculations and shows final number
+    
+ 
+*/
 
-function operate(){
+const numPad = document.querySelectorAll(".num");
+const display = document.querySelector(".display");
+const operators = document.querySelectorAll(".operator");
 
+operators.forEach(sign => {
+    sign.addEventListener("click", () => {
+
+    });
+});
+
+numPad.forEach(num => {
+    num.addEventListener("click", () => {
+        display.innerHTML = num.innerHTML; 
+    });
+});
+
+function operate(a, sign, b){
+    switch (sign) {
+        case "+":
+            return
+    }
 }
