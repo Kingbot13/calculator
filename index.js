efaebe4d48796256742f766c9690941c14ae2c85
Dylan King <dwkblue51@yahoo.com>
2021-06-22 14:04:22 -0500

    add buttons to be used instead of using divs by themselves

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const numPad = document.querySelectorAll(".num");
+const numPad = document.querySelectorAll(".num-btn");
 const display = document.querySelector(".display");
-const operators = document.querySelectorAll(".operator");
+const operators = document.querySelectorAll(".oper-btn");
 const clearAll = document.querySelector(".ac");
+let numArray = [];
+
+function clear(){
+    display.innerHTML = "0";
+    numArray = [];
+};
 
 function add([...args]){
     console.log(args);
@@ -23,39 +29,34 @@ function multiply([...args]){
 };
 
 function divide([...args]){
-    let results = args[0] / args[1];
+    let results;
+    args[1] === 0 ? alert("Tsk tsk tsk. You thought you were clever trying to divide by zero!")
+    :
+    results = args[0] / args[1];
     display.innerHTML = results;
     return results;
 };
 
 
-let numArray = [];
-
-
-function clear(){
-    display.innerHTML = "0";
-    numArray = [];
-};
-
 clearAll.addEventListener("click", clear); // set display to zero and reset numArray
+
 
 operators.forEach(sign => {
     sign.addEventListener("click", () => {
         let numSlice;
+        numArray.push(parseFloat(display.innerHTML));
+        numArray.push(sign.innerHTML);
+        display.innerHTML = "0";
         if (numArray.length >= 3){
             numSlice = numArray.slice(0, 3);
-            console.log(numSlice);
+            console.log("slice:",numSlice);
             operate(numSlice);
         } else if (sign === "=" && numArray.length >= 3) {
             numSlice = numArray.slice(0, 3);
 
             operate(numSlice);
-        }else{
-            numArray.push(parseFloat(display.innerHTML));
-            numArray.push(sign.innerHTML);
-            display.innerHTML = "0";
-            console.log(numArray);
-        };  
+        };
+            console.log(numArray);  
     });
 });
 
@@ -63,6 +64,9 @@ numPad.forEach(num => {
     num.addEventListener("click", () => {
         if (display.innerHTML === "0"){
             display.innerHTML = num.innerHTML; 
+        }else if(numArray.length === 2){
+            display.innerHTML = num.innerHTML;
+
         } else {
             display.innerHTML = display.innerHTML + num.innerHTML;
         };
@@ -70,11 +74,10 @@ numPad.forEach(num => {
 });
 
 /* 
-    after calculation, remove first three indexes in numArray and replace with result from calculation 
+    
 */
 
 function operate([...args]){
-    
     switch (args[1]) {
         case "+":
             args.splice(1, 1);
@@ -88,7 +91,7 @@ function operate([...args]){
             args.splice(1, 1);
             divide(args);
             break;
-        case "X":
+        case "x":
             args.splice(1, 1);
             multiply(args);
             break;
@@ -96,5 +99,6 @@ function operate([...args]){
             alert("Wrong input");
             break;
     };
-
+    numArray.splice(0, 3);
+    numArray.unshift(parseFloat(display.innerHTML));
 };
