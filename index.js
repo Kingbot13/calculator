c4a2985ed5a16daa6736598b373349e6f5aa5164
Dylan King <dwkblue51@yahoo.com>
2021-06-25 21:59:47 -0500

    use displayNum in place of display.innerHTML to reduce conversions between strings and int

--- index.js
+++ index.js
@@ -6,23 +6,38 @@ const decimal = document.querySelector("#decimal");
 const percent = document.querySelector(".percent");
 const signChange = document.querySelector(".change-sign")
 const equalSign = document.querySelector("#equal-sign");
+
 let numArray = []; // store numbers and operators
+let displayNum;
+
+function updateDisplay(){
+    display.innerHTML = displayNum;
+}
 
 percent.addEventListener("click", () => {
-    display.innerHTML /= 100; 
+    displayNum /= 100; 
+    updateDisplay();
 });
 
 signChange.addEventListener("click", () => {
-    display.innerHTML *= -1;
+    displayNum *= -1;
+    updateDisplay();
 });
 
-function hasDecimal(){
-    if (display.innerHTML - Math.floor(display.innerHTML) !== 0){
+function hasDecimal(){ // disables decimal if already exists in the display
+    if (displayNum - Math.floor(displayNum) !== 0){
         decimal.disabled = true;
     } else {
         decimal.disabled = false;
     };
 };
+
+decimal.addEventListener("click", () => { // decimal control... needs work
+    hasDecimal();
+    displayNum += decimal.innerHTML;
+    updateDisplay();
+
+});
 
 function hasEqualSign(){
     if (numArray[1] === "="){
@@ -34,8 +49,9 @@ function hasEqualSign(){
 };
 
 function clear(){
-    display.innerHTML = "0";
+    displayNum = 0;
     numArray = [];
+    updateDisplay();
 };
 
 
@@ -51,14 +67,14 @@ operators.forEach(sign => {
             numArray.splice(0, 2);
         };
         
-        numArray.push(parseFloat(display.innerHTML));
-        display.innerHTML = 0; // resets display after operator button pressed
+        numArray.push(displayNum);
+        displayNum = 0; // resets display after operator button pressed
         numArray.push(sign.innerHTML);
         
         if (hasEqualSign()) {
             console.log("equals", numArray);
             numArray.splice(0, 2);
-            display.innerHTML = 0;
+            displayNum = 0;
         } else {
             if (numArray.length >= 3){
                 numSlice = numArray.slice(0, 3);
@@ -67,7 +83,7 @@ operators.forEach(sign => {
             };
             
         };
-        
+        updateDisplay();
         console.log(numArray);  
     });
 }); 
@@ -79,55 +95,63 @@ display + num
 */
 
 numPad.forEach(num => {
+    let counter = 0;
+    if (numArray === 2){
+        counter++;
+    };
     num.addEventListener("click", () => {
         hasDecimal();
-        let counter = 0;
+        let number = parseFloat(num.innerHTML);
+    
+
         if (display.innerHTML === "0" || display.innerHTML === 0){
-            if (num.innerHTML === "."){
-                display.innerHTML = display.innerHTML + num.innerHTML;
-                
-            } else {
-                display.innerHTML = num.innerHTML; 
-            };
+         
+            displayNum = number; 
+            console.log("condition 1");
+            
         }else if(hasEqualSign()){
-            display.innerHTML = num.innerHTML;
+            displayNum = number;
             numArray.splice(0,2);
+            console.log("condition 2");
             
-            // }else if(counter === 0){
-                
-                //     counter++;
-                //     display.innerHTML = num.innerHTML;
-                //     console.log("counter", counter);
-                
-                
-            }else {
-                display.innerHTML = display.innerHTML + num.innerHTML;
-            };
+        }else if(counter === 0 && numArray.length === 2){ // counter counts # of times num clicked
             
-            if (numArray.length !== 2){
-                counter = 0;
-            };
-            console.log("current counter:", counter);
-        });
+            counter++;
+            displayNum = number;
+            console.log("counter", counter);
+            console.log("condition 3");
+            
+        }else {
+            displayNum = displayNum * 10 + number;
+            console.log("condition 4");
+        };
+        
+        if (numArray.length !== 2){
+            counter = 0;
+        };
+        console.log("array length:", numArray);
+        console.log("current counter:", counter);
+        updateDisplay();
     });
+});
     
 function operate([...args]){
     function add([...args]){
         // console.log(args);
         let results = args[0] + args[1];
-        display.innerHTML = results;
+        displayNum = results;
         return results;
     };
     
     function subtract([...args]){
         let results = args[0] - args[1];
-        display.innerHTML = results;
+        displayNum = results;
         return results;
     };
     
     function multiply([...args]){
         let results = args[0] * args[1];
-        display.innerHTML = results;
+        displayNum = results;
         return results;
     };
     
@@ -136,7 +160,7 @@ function operate([...args]){
         args[1] === 0 ? alert("Tsk tsk tsk. You thought you were clever trying to divide by zero!")
         :
         results = args[0] / args[1];
-        display.innerHTML = results;
+        displayNum = results;
         return results;
     };
 
@@ -145,14 +169,17 @@ function operate([...args]){
         args.splice(1, 1);
         add(args);
         break;
+
         case "-":
         args.splice(1, 1);
         subtract(args);
         break;
+
         case "/":
         args.splice(1, 1);
         divide(args);
         break;
+        
         case "x":
         args.splice(1, 1);
         multiply(args);
@@ -164,6 +191,7 @@ function operate([...args]){
             alert("Wrong input");
             break;
         };
+    updateDisplay();
     numArray.splice(0, 3);
-    numArray.unshift(parseFloat(display.innerHTML));
+    numArray.unshift(parseFloat(displayNum));
 };
